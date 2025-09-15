c0291a943de4d6f809db674af7990b37f34bb187
yoon255 <yjhclass001@gmail.com>
2025-09-15 19:54:23 +0900

    .

--- Assets/Scripts/TestLoop.cs
+++ Assets/Scripts/TestLoop.cs
@@ -3,50 +3,41 @@ using UnityEngine;
 public class TestLoop : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Start()    // 함수(Method) -> 프로그램을 실행하면 처음에 실행되는 함수
     {
-        // ++ : 값을 1씩 증가시켜주는 증감 연산자.
-        // -- : 값을 1씩 감소시켜주는 증감 연산자.
-        // for
-        //  초기식;조건식;증감식.
-        for (int i = 0; i < 10; i++)
-        {
-            Debug.Log("i = " + i);
-            if(i == 5)
-            {
-                break;
-            }
-        }
-
-        bool isAlive = true;
-        int hp = 100;
-        while(hp > 0)
-        {
-            Debug.Log("나는 살아있다!!!!");
-            hp = hp - 10;
-            
-            if(hp <= 50)
-            {
-                break;
-            }
-        }
-
-        Debug.Log("나는 죽었다~~~~ " + hp);
-
-        // 1 -> 00000001
-        // 2 -> 00000010
-        // 3 -> 00000011
-        // 4 -> 00000100
-        // 5 -> 00000101
-        // 6 -> 00000110
-        // 7 -> 00000111
-        // 8 -> 00001000
-        // 비트연산.
+        PrintDebugLog();
+        PrintDebugLog("Hello");
+        string message = GetString();
+        Debug.Log(message);
+        string message2 = GetString("hi");
+        Debug.Log(message2);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void PrintDebugLog()
+    {
+        Debug.Log("PrintDebug>og()");
+    }
+
+    void PrintDebugLog(string message)
+    {
+        Debug.Log(message);
+    }
+
+    string GetString()
+    {
+        string message = "Hello";
+        return message;
+    }
+
+    string GetString(string message)
+    {
+        string result = "aaa" + message;
+        return result;
     }
 }
