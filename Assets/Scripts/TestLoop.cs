51fab8e4397f41d61e0b21f691c2a0bbe67aa0c5
yoon255 <yjhclass001@gmail.com>
2025-09-16 20:51:16 +0900

    ,

--- Assets/Scripts/TestLoop.cs
+++ Assets/Scripts/TestLoop.cs
@@ -1,43 +1,46 @@
+using NUnit.Framework;
+using System;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class TestLoop : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()    // 함수(Method) -> 프로그램을 실행하면 처음에 실행되는 함수
     {
-        PrintDebugLog();
-        PrintDebugLog("Hello");
-        string message = GetString();
-        Debug.Log(message);
-        string message2 = GetString("hi");
-        Debug.Log(message2);
+        List<int> item = new List<int>();
+        item.Add(1);    // 데이터 추가.
+        item.Add(2);
+        item.Add(3);
+        item.Add(4);
+        item.Add(5);
+
+        int index = item.IndexOf(10);
+        bool ok = item.Contains(5);
+        Debug.Log(ok);
+
+        item.Insert(1, 6);
+
+        for (int i = 0; i < item.Count; ++i)
+        {
+            Debug.Log(item[i]);
+        }
+
+        item.Remove(3);
+        item.RemoveAt(0);
+
+        for (int i = 0; i < item.Count; ++i)
+        {
+            Debug.Log(item[i]);
+        }
+
+        item.Clear();
+        Debug.Log("count = " + item.Count);
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void PrintDebugLog()
-    {
-        Debug.Log("PrintDebug>og()");
-    }
-
-    void PrintDebugLog(string message)
-    {
-        Debug.Log(message);
-    }
-
-    string GetString()
-    {
-        string message = "Hello";
-        return message;
-    }
-
-    string GetString(string message)
-    {
-        string result = "aaa" + message;
-        return result;
     }
 }
